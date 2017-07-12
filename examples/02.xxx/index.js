f37b2f9a3f793a5e94e7f4f2fc83c65bcad659bc
_gt3 <gt3.someday@outlook.com>
2017-07-11 22:17:03 -0500

    test example

--- examples/02.xxx/index.js
+++ examples/02.xxx/index.js
@@ -1,10 +1,20 @@
 import React, { Component, createElement } from 'react'
 import PropTypes from 'prop-types'
 import { render } from 'react-dom'
-import { a, spec, check, match, prefixMatch, container, appendPath, parseQS } from 'ultra'
+import { spec, check, match, prefixMatch, appendPath, parseQS } from 'ultra'
 
-let App = () => {
-  return <p>02.xxx app</p>
+let App = props => <div>
+    <p>{props.label}</p>
+    <App.a href={App.pathKey + '/a'}>a-link</App.a>
+  </div>
+
+let _renderApp = node => label => render(<App label={label} />, node)
+
+export default (node, runUltra, replaceMatchers, A, pathKey) => {
+  App.a = A
+  App.pathKey = pathKey
+  let renderApp = _renderApp(node)
+  let amatch = prefixMatch(pathKey, match(spec('/a')(() => renderApp('aaa'))))
+  runUltra(curr => [...curr, amatch])
+  return () => renderApp('xxx')
 }
-
-export default (node, runUltra) => render(<App />, node)
